6dd218984db028c32af886562e391c7b748197a7
Rani Pawar <pawar.rani241996@gmail.com>
2024-02-02 08:17:47 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
-import IncrimentandDecrement from "./Components/IncrimentandDecrement";
-// import Subscription from "./Components/Subscription";
 
 function App() {
-  // let subscriptions = [
-  //   {
-  //     id: "1",
-  //     date: new Date("2021", "03", "23"),
-  //     title: "Monthly Subscription",
-  //     amount: "125.60",
-  //   },
-  //   {
-  //     id: "1",
-  //     date: new Date("2021", "03", "23"),
-  //     title: "Monthly Subscription",
-  //     amount: "125.60",
-  //   },
-  // ];
-
-  // let date = new Date("2021", "03", "23");
-  // let title = "Monthly Subscription";
-  // let amount = "125.60";
+  const x = 10;
   return (
-    <div>
-      <IncrimentandDecrement />
-    </div>
+    <>
+      <h1 className="x">Hello world!!</h1>
+      <p>{x < 15 ? "Greater" : "Smaller"}</p>
+    </>
+
+    // <div>
+    //   <IncrimentandDecrement />
+    // </div>
   );
 }
 
--- src/Components/IncrimentandDecrement.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function IncrimentandDecrement() {
-  return <h2>Hey my name is Raaa</h2>;
-}
-
-export default IncrimentandDecrement;
