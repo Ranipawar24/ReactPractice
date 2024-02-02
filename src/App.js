3a2f34ac407580b6a76f37f4ec197c4873a1908e
Rani Pawar <pawar.rani241996@gmail.com>
2024-02-02 08:05:41 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import IncrimentandDecrement from "./Components/IncrimentandDecrement";
+// import Subscription from "./Components/Subscription";
 
 function App() {
+  // let subscriptions = [
+  //   {
+  //     id: "1",
+  //     date: new Date("2021", "03", "23"),
+  //     title: "Monthly Subscription",
+  //     amount: "125.60",
+  //   },
+  //   {
+  //     id: "1",
+  //     date: new Date("2021", "03", "23"),
+  //     title: "Monthly Subscription",
+  //     amount: "125.60",
+  //   },
+  // ];
+
+  // let date = new Date("2021", "03", "23");
+  // let title = "Monthly Subscription";
+  // let amount = "125.60";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <IncrimentandDecrement />
     </div>
   );
 }
---
+++ src/Components/IncrimentandDecrement.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function IncrimentandDecrement() {
+  return <h2>Hey my name is Raaa</h2>;
+}
+
+export default IncrimentandDecrement;
