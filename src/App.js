2f69c612704f8c3771fac38375f4437dd5b3aa89
Rani Pawar <pawar.rani241996@gmail.com>
2024-02-03 12:29:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,11 +5,10 @@ function App() {
   const x = 10;
   return (
     <>
-      /* <h1 className="x">Hello world!!</h1>
-      <p>{x < 15 ? "Greater" : "Smaller"}</p>*/
-      <Header name="Rani" />
-      <Header name="Rani@123" />
-      <Header name="Rani@2020.com" />
+      // /* <h1 className="x">Hello world!!</h1>
+      // <p>{x < 15 ? "Greater" : "Smaller"}</p>*/ // <Header name="Rani" />
+      // <Header name="Rani@123" />
+      // <Header name="Rani@2020.com" />
     </>
 
     // <div>
@@ -25,3 +24,9 @@ export default App;
 
 // }
 // addEventListener(5)
+
+// // RULES OF HOOKS
+// 1 MUST BE WRITTEN INSIDE FUNCTION COMPONENT
+// 2 MUST BE IMPORT IN COMPONENT
+// 3 MUST BE CALL AT TOP LEVEL OF COMP
+// 4 CANNOT BE CONDITIONAL
