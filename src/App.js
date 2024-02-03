b4c53e03af632f083b4e6fc8fa4e6e10e4202ae4
Rani Pawar <pawar.rani241996@gmail.com>
2024-02-03 12:35:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 import Header from "./Components/Header";
 
 function App() {
-  const x = 10;
   return (
     <>
-      // /* <h1 className="x">Hello world!!</h1>
-      // <p>{x < 15 ? "Greater" : "Smaller"}</p>*/ // <Header name="Rani" />
-      // <Header name="Rani@123" />
-      // <Header name="Rani@2020.com" />
+      <h1>Learning Usestate</h1>
+
+      {/* <h1 className="x">Hello world!!</h1>
+      <p>{x < 15 ? "Greater" : "Smaller"}</p>
+      <Header name="Rani" />
+      <Header name="Rani@123" />
+      <Header name="Rani@2020.com" />*/}
     </>
 
     // <div>
