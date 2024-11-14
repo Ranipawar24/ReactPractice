9218a717ba26b7f6c4f2c5257df93706de93d1b2
Rani Pawar <pawar.rani241996@gmail.com>
2024-11-14 11:12:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import "./App.css";
 
-import { useState, React } from "react";
+import { useState, useEffect, React } from "react";
 
 function App() {
   /* EXAMPLE OF USESTATE*/
   const [count, setcounter] = useState(0);
+  const [data, setData] = useState("Ram");
+
+  useEffect(() => {
+    console.log("component mounted");
+  }, [data]);
+
   function updateCount() {
     setcounter(count + 1);
   }
+  function updateData() {
+    setData("seeta");
+  }
+
   return (
     <>
       <h1>Learning Usestate</h1>
       <h2>Button clicked {count} times </h2>
       <button onClick={updateCount}>Click Me</button>
+      <button onClick={updateData}>Update Data</button>
     </>
   );
 }
